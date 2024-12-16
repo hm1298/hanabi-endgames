0c56e26c4d3b371aa37840312d13627216ffceaf
hm1298 <31015794+hm1298@users.noreply.github.com>
2024-12-16 15:14:26 -0500

    feat(core): create Deck and Card classes and refactor methods

--- endgames/game/util.py
+++ endgames/game/util.py
@@ -15,72 +15,79 @@ def lookup_variant(variant_name):
     """
     return VARIANT_NAMES_DICT[variant_name]
 
-def init_deck(variant: Variant):
-    deck = []
-    for suit_index, suit in enumerate(variant.suits):
-        for rank in variant.clue_ranks:
-            card = create_card(suit_index, rank)
+class Deck:
+    """An ordered list of cards"""
+    def __init__(self, variant=None):
+        if variant is None:
+            variant = lookup_variant("No Variant")
+        elif isinstance(variant, str):
+            variant = lookup_variant(variant)
 
-            if variant.stack_size == 4 and rank == 5:
-                continue
-            deck.append(card)
-            if suit.one_of_each:
-                continue
-            if variant.sudoku:
-                deck.append(card)
-            elif rank == 1:
-                if variant.up_or_down or suit.reversed:
+        self.seed = None
+        self.variant = variant
+        self._init_deck(variant)
+
+    def _init_deck(self, variant: Variant):
+        deck = []
+        for suit_index, suit in enumerate(variant.suits):
+            for rank in variant.clue_ranks:
+                card = Card(suit_index, rank)
+
+                if variant.stack_size == 4 and rank == 5:
                     continue
                 deck.append(card)
-                deck.append(card)
-            elif rank == variant.critical_rank:
-                continue
-            elif rank == 5:
-                if suit.reversed:
+                if suit.one_of_each:
+                    continue
+                if variant.sudoku:
+                    deck.append(card)
+                elif rank == 1:
+                    if variant.up_or_down or suit.reversed:
+                        continue
                     deck.append(card)
                     deck.append(card)
-            else:
-                deck.append(card)
+                elif rank == variant.critical_rank:
+                    continue
+                elif rank == 5:
+                    if suit.reversed:
+                        deck.append(card)
+                        deck.append(card)
+                else:
+                    deck.append(card)
 
-    return deck
+        self.deck = deck
 
-def get_deck(variant_name, seed):
-    variant = lookup_variant(variant_name)
-    deck = init_deck(variant)
-    return shuffle_deck(seed, deck)
+    def print(self, cutoff=None):
+        if cutoff is None:
+            cutoff = len(self.deck)
+        print(" ".join([str(card.interpret()) for card in self.deck[:cutoff]]))
 
-def print_deck(deck, cutoff=None):
-    if cutoff is None:
-        cutoff = len(deck)
-    print(" ".join([str(interpret_card(card)) for card in deck[:cutoff]]))
+    # TODO: implement Hanab Live shuffling method
+    def shuffle(self, seed):
+        """Shuffles deck according to a seed.
 
-# TODO: implement Hanab Live shuffling method
-def shuffle_deck(seed, deck):
-    """Shuffles deck according to a seed.
+        Args:
+            seed (str): a seed string as used on Hanab Live
+            deck (list): a list of cards
 
-    Args:
-        seed (str): a seed string as used on Hanab Live
-        deck (list): a list of cards
+        Returns:
+            list: a copy of deck sorted by seed
+        """
+        self.seed = seed
+        random.seed(seed)
+        random.shuffle(self.deck)
 
-    Returns:
-        list: a copy of deck sorted by seed
-    """
-    result = deck[:]
-    print(len(result))
-    print(seed)
-    random.seed(seed)
-    random.shuffle(result)
-    return result
-
-def create_card(suit_index, rank):
-    return (suit_index << 31) | rank
-
-def interpret_card(n):
-    x = n >> 31
-    y = n & 0x7FFFFFFF
-    return x, y
+class Card:
+    """A card with suit and rank"""
+    def __init__(self, suit_index, rank):
+        self.value = (suit_index << 31) | rank
+    def interpret(self):
+        x = self.value >> 31
+        y = self.value & 0x7FFFFFFF
+        return x, y
 
 if __name__ == "__main__":
     VAR = "No Variant"
     SEED = "p2v0scommendation-splintering-gondolas"
-    print_deck(get_deck(VAR, SEED))
+    DECK = Deck(VAR)
+    DECK.shuffle(SEED)
+    DECK.print()
